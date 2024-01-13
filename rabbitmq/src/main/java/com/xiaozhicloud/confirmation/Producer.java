5545b7624e53bd295b8e36ec478cac2265ba54b8
dezhizhang <1541609448@qq.com>
2024-01-13 19:19:42 +0800

    feat: 批量确认

--- rabbitmq/src/main/java/com/xiaozhicloud/confirmation/Producer.java
+++ rabbitmq/src/main/java/com/xiaozhicloud/confirmation/Producer.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 
 public class Producer {
   public static void main(String[] args) throws Exception {
-    publishMessageIndividually();
+    publishMessageBatch();
   }
 
   public static void publishMessageIndividually() throws Exception {
@@ -32,6 +32,31 @@ public class Producer {
     long end = System.currentTimeMillis();
     System.out.println("用时:"+ (end - begin));
 
+  }
+  public static void publishMessageBatch() throws Exception {
+    Channel channel = Utils.getChannel();
+
+    String queueName = UUID.randomUUID().toString();
+    channel.queueDeclare(queueName,true,false,false,null);
+
+    // 开启发布确认
+    channel.confirmSelect();
+
+    // 开始时间
+    long begin = System.currentTimeMillis();
+
+    // 批量确认消息大小
+    int batchSize = 100;
+
+    for(int i=0;i < 1000;i++) {
+      String message = i + "";
+      channel.basicPublish("",queueName,null,message.getBytes());
+      if(i % batchSize == 0) {
+        channel.waitForConfirms();
+      }
+    }
+    long end = System.currentTimeMillis();
+    System.out.println("用时:"+ (end - begin));
 
   }
 
