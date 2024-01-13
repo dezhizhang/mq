133ebc9be4cb33946f326fb0ae6facc10c5105cd
dezhizhang <1541609448@qq.com>
2024-01-13 13:47:38 +0800

    feat: 消息

--- rabbitmq/src/main/java/com/xiaozhicloud/consumer/Consumer.java
+++ rabbitmq/src/main/java/com/xiaozhicloud/consumer/Consumer.java
@@ -1,2 +1,40 @@
-package com.xiaozhicloud.consumer;public class Consumer {
+package com.xiaozhicloud.consumer;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.*;
+
+public class Consumer {
+  public static final String QUEUE_NAME = "hello";
+
+  public static void main(String[] args) throws Exception {
+    // 创建链接工厂
+    ConnectionFactory factory = new ConnectionFactory();
+    //设置ip地址
+    factory.setHost("127.0.0.1");
+    // 设置用户名
+    factory.setUsername("guest");
+    // 设置密码
+    factory.setPassword("guest");
+
+    // 创建连接
+    Connection connection = factory.newConnection();
+    // 创建信道
+
+    Channel channel = connection.createChannel();
+    DeliverCallback deliverCallback = (consumerTag,message) -> {
+      System.out.println("接收消息"+ new String(message.getBody()));
+    };
+
+    CancelCallback cancelCallback = consumerTag -> {
+      System.out.println("消费消息被中断");
+    };
+
+    // 消费者消费消息
+    channel.basicConsume(QUEUE_NAME,true,deliverCallback,cancelCallback);
+
+
+  }
+
 }
--- rabbitmq/src/main/java/com/xiaozhicloud/producer/Producer.java
+++ rabbitmq/src/main/java/com/xiaozhicloud/producer/Producer.java
@@ -1,2 +1,35 @@
-package com.xiaozhicloud.producer;public class Producer {
+package com.xiaozhicloud.producer;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Producer {
+  public static final String QUEUE_NAME = "hello";
+
+  public static void main(String[] args) throws Exception {
+
+    // 创建链接工厂
+    ConnectionFactory factory = new ConnectionFactory();
+
+    // 设置ip地址
+    factory.setHost("127.0.0.1");
+    // 设置用户名
+    factory.setUsername("guest");
+    // 设置密码
+    factory.setPassword("guest");
+
+    // 创建连接
+    Connection connection = factory.newConnection();
+    // 生成队列
+    Channel channel = connection.createChannel();
+
+    channel.queueDeclare(QUEUE_NAME,false,false,false,null);
+    // 发送消息
+    String message = "hello world";
+    channel.basicPublish("",QUEUE_NAME,null,message.getBytes());
+    System.out.println("消息发送完毕");
+
+  }
+
 }
